81cb9a37e44c713b80c5a2c26e1473f55f5874c4
Aled Cuda <aledvirgil@gmail.com>
2021-02-20 16:15:34 -0800

    space?

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ ARM_TOOLCHAIN="https://armkeil.blob.core.windows.net/developer/Files/downloads/g
 container=$(buildah from fedora)
 mount=$(buildah mount $container)
 
+echo
+echo "INSTALLING base packages:"
 buildah run $container -- dnf install \
 	bash coreutils python3 python3-pip git mercurial rust \
 	bash-completion cmake gperf dfu-util dtc tar wget curl \
@@ -11,20 +13,31 @@ buildah run $container -- dnf install \
 	python3-intelhex cgdb ncurses \
 	--setopt install_weak_deps=false -y
 
-echo "Downloading ARM toolchain from $ARM_TOOLCHAIN"
+echo
+echo "DOWNLOADING ARM toolchain from $ARM_TOOLCHAIN:"
 buildah run $container -- curl $ARM_TOOLCHAIN -o /opt/toolchain.tar.bz2
 buildah run $container -- bash -c "cd /opt && tar jxf toolchain.tar.bz2 && rm toolchain.tar.bz2 && mv * arm-toolchain"
 
+echo
+echo "CONFIGURING environment:"
 buildah run $container -- bash -c "echo 'PATH=$PATH:/opt/arm-toolchain/bin/;export PATH' >> /etc/profile"
 
 buildah run $container -- bash -c "cp /etc/skel/.bashrc /root/ && chmod 755 /root/.bashrc"
 
+echo
+echo "INSTALLING MBED:"
 buildah run $container -- python3 -m pip --no-input install mbed-cli
 buildah run $container -- mbed config -G GCC_ARM_PATH "/opt/arm-toolchain/bin"
 buildah run $container -- python3 -m pip --no-input install jsonschema mbed_cloud_sdk \
 	mbed_ls mbed_host_tests mbed_greentea manifest_tool icetea pycryptodome
 
-buildah commit $container star-toolchain-nozephyr
+
+echo
+echo "BUILT star-toolchain-nozephyr"
+echo "COMMITING to star-toolchain-nozephyr:"
+df -h
+#buildah commit $container star-toolchain-nozephyr
+df -h
 
 buildah run $container -- dnf install \
 	ccache dtc SDL2-devel python3-tkinter ninja-build \
@@ -35,8 +48,10 @@ buildah run $container -- west init /opt/zephyrproject
 buildah run $container -- bash -c "cd /opt/zephyrproject && west update && west zephyr-export"
 buildah run $container -- pip3 install -r /opt/zephyrproject/zephyr/scripts/requirements.txt
 
-buildah run $container -- bash -c "curl 'https://gist.githubusercontent.com/ld-cd/16e2a0669d5c20b430753cfca5432a6a/raw/1fc6296f5d2816d994446a536ff64edfbf4bd656/.zypherrc' >> /etc/profile'"
+buildah run $container -- bash -c "curl 'https://gist.githubusercontent.com/ld-cd/16e2a0669d5c20b430753cfca5432a6a/raw/1fc6296f5d2816d994446a536ff64edfbf4bd656/.zypherrc' >> /etc/profile"
 
 echo
-echo "Commiting to star-toolchain:"
+echo "COMMITING to star-toolchain:"
+df -h
 buildah commit $container star-toolchain
+df -h
